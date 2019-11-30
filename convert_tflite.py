e5e43443e5e32ee50306164df1b41f5b2efe9f76
Felix Dittrich <felixdittrich92@gmail.com>
2019-11-30 21:07:18 +0100

    script

--- convert_tflite.py
+++ convert_tflite.py
@@ -2,14 +2,22 @@ import tensorflow as tf
 from tensorflow.keras.models import load_model
 
 # .h5 to .tflite
+"""
 new_model= tf.keras.models.load_model(filepath="./data/traffic_signs_20_epochs.h5")
 tflite_converter = tf.lite.TFLiteConverter.from_keras_model(new_model)
 tflite_model = tflite_converter.convert()
 open("./data/tf_lite_model.tflite", "wb").write(tflite_model)
+"""
 
-""" .pb to .tflite
-model.save(filepath="save_model/")
-converter = tf.lite.TFLiteConverter.from_saved_model('save_model')
+#.pb to .tflite
+"""
+graph_def_file = "frozen_inference_graph.pb"
+
+converter = tf.lite.TFLiteConverter.from_frozen_graph(
+        graph_def_file, input_arrays, output_arrays)
+tflite_model = converter.convert()
+open("traffic_model.tflite", "wb").write(tflite_model)
+"""
+converter = tf.lite.TFLiteConverter.from_saved_model('models/')
 tflite_model = converter.convert()
 open("converted_model.tflite", "wb").write(tflite_model)
-"""
