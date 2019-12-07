4032c29a3770ff334217e97831b2a5674038474c
Felix Dittrich <felixdittrich92@gmail.com>
2019-12-07 10:32:45 +0100

    converter

--- object_detection_API/models/conert_to_tflite.py
+++ object_detection_API/models/conert_to_tflite.py
@@ -6,10 +6,10 @@ for n in gf.node:
 
 
 graph_def_file = "traffic_graph.pb"
-input_arrays = ["image_tensor"] #ToDo
+input_arrays = ["image_tensor"] 
 output_arrays = ['num_detections', 'detection_boxes', 'detection_scores', 'detection_classes']
 
 converter = tf.compat.v1.lite.TFLiteConverter.from_frozen_graph(
-        graph_def_file, input_arrays, output_arrays)
+        graph_def_file, input_arrays, output_arrays, input_shapes={"image_tensor":[1,800,600,3]})
 tflite_model = converter.convert()
 open("converted_model.tflite", "wb").write(tflite_model)
